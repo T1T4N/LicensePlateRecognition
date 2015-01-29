f84f5f190f7af0e6943ab647c3746e9a68a3cf79
makedon4e <martinboncanoski@gmail.com>
2015-01-29 18:05:05 +0100

    New detector, Tesseract OCR

--- detector/ThresholdDetector.py
+++ detector/ThresholdDetector.py
@@ -5,7 +5,7 @@ from detector import AbstractDetector
 from utils import loader, display
 
 
-class ThresholdBlurDetector(AbstractDetector):
+class ThresholdDetector(AbstractDetector):
 
     def __init__(self, image):
         if type(image) == str:
@@ -18,8 +18,7 @@ class ThresholdBlurDetector(AbstractDetector):
     def _check_sizes(self, candidate):
         return True
 
-
-    def find_rectangles_martin(self):
+    def find_rectangles(self):
         """Finds the contours which are convex rectangles
         :returns The a list of all rectangle contours"""
 
@@ -71,53 +70,41 @@ class ThresholdBlurDetector(AbstractDetector):
 
         return rectangles
 
+    def text_detection_mser(img_name):
+        # finding text in an image with MSER
+        img=cv2.imread(img_name)
+        mser = cv2.MSER(_min_area=150, _max_area=2000)
 
-    def find_rectangles(self):
-        """Finds the contours which are convex rectangles
-        :returns The a list of all rectangle contours"""
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        vis = img.copy()
 
-        # create a grayscale version of the image
-        gray_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        regions = mser.detect(gray, None)
+        mserRegionsPixels = np.concatenate(regions, axis=0)
 
-        # Blur the image
-        gray_img = cv2.adaptiveBilateralFilter(gray_img, (11, 11), 100)
-        # gray_img = cv2.bilateralFilter(gray_img, 5, 100, 100)
-        # gray_img = cv2.blur(gray_img, (5, 5))
-        if __debug__:
-            display.show_image(gray_img, 'Gray')
+        mserMask = np.zeros((len(gray), len(gray[0]))) # ne mozi so dtype=int izleguva cela slika crna
+        ind = np.add(mserRegionsPixels[:,1],(mserRegionsPixels[:,0]-1)*len(mserMask))
+        for i in ind:
+            length = len(mserMask)
+            col = int(i/length)
+            row = i - (col*length)
+            col += 1
+            mserMask[row, col] = 1
 
-        thresh = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 17, 2)
 
-        # gaussian 11, (3,3), 0: 4 tablicki od 6 sliki
-        # gaussian 21, (3,3), 0: 8 tablicki od 13 sliki
-        # gaussian 17, (3,3), 0: 10 tablicki od 13 sliki
+        # hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+        # cv2.polylines(gray, hulls, 1, (0, 255, 0))
+        # print mserMask.shape
+        edge = cv2.Canny(gray, 255, 255)
 
-        blurred = cv2.GaussianBlur(thresh, (3, 3), 0)
+        edgeAndMSERIntersection = np.zeros((len(edge), len(edge[0])))
+        for i in range(len(edge)):
+            for j in range(len(edge[0])):
+                if edge[i, j] >= 1 and mserMask[i, j] == 1:
+                    edgeAndMSERIntersection[i, j] = 1
 
-        if __debug__:
-            display.show_image(blurred, 'Blurred')
+        #dialtion
+        kernel = np.ones((1,1),np.uint8)
+        closing = cv2.morphologyEx(edgeAndMSERIntersection, cv2.MORPH_OPEN, kernel)
 
-        edges = cv2.Canny(blurred, 100, 100, 3)
-        if __debug__:
-            display.show_image(edges, 'Canny edges')
-
-        contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        if __debug__:
-            self.display_contours(contours)
-
-        biggest = None
-        max_area = 0
-        rectangles = []
-        for i in contours:
-            area = cv2.contourArea(i)
-            if area > 50:
-                peri = cv2.arcLength(i, True)
-                approx = cv2.approxPolyDP(i, 0.02*peri, True)
-
-                if len(approx) == 4 and cv2.isContourConvex(approx):
-                    rectangles.append(approx)
-                    if area > max_area:
-                        biggest = approx
-                        max_area = area
-
-        return rectangles
+        cv2.imshow('img', closing )
+        cv2.waitKey(0)
--- main/main.py
+++ main/main.py
@@ -1,10 +1,13 @@
 import os
-
+import numpy as np
 import cv2
+import tesseract
+import cv2.cv as cv
 
-from detector import MorphologyTransformDetector, ThresholdBlurDetector
+from detector import MorphologyTransformDetector, ThresholdBlurDetector, ThresholdDetector
 from utils.loader import load_images
 from utils.display import display_rectangles
+from utils import display
 
 
 def get_images_from_dir(directory):
@@ -14,6 +17,116 @@ def get_images_from_dir(directory):
             ret.append(directory + os.sep + file_name)
     return ret
 
+def process_image(img):
+    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # cv2.bitwise_not(img, img)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.adaptiveBilateralFilter(gray, (7, 7), 15)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+    display.show_image(thresh, 'White')
+
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = (0,0,0)
+    upper = (0,0,255)
+
+    lower = np.array(lower, dtype = "uint8")
+    upper = np.array(upper, dtype = "uint8")
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(img, img, mask = mask)
+    # ret,gray = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+    # gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+    display.show_image(output, 'White')
+
+
+def tesseract(img_name):
+    #tries to find text in ana image
+    #the image must contain only letters, else it will fail to recognize text
+
+    image0=cv2.imread(img_name)
+
+    #### you may need to thicken the border in order to make tesseract feel happy to ocr your image #####
+    offset=20
+    height,width,channel = image0.shape
+
+    image1=cv2.copyMakeBorder(image0,offset,offset,offset,offset,cv2.BORDER_CONSTANT,value=(255,255,255))
+    cv2.namedWindow("Test")
+    cv2.imshow("Test", image1)
+    cv2.waitKey(0)
+    cv2.destroyWindow("Test")
+
+    #####################################################################################################
+    api = tesseract.TessBaseAPI()
+    api.Init(".","eng",tesseract.OEM_DEFAULT)
+    api.SetPageSegMode(tesseract.PSM_AUTO)
+    height1,width1,channel1=image1.shape
+    print image1.shape
+    print image1.dtype.itemsize
+    width_step = width*image1.dtype.itemsize
+    print width_step
+
+    image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    # Apply Otsu's Binary Thresholding
+    ret, image1 = cv2.threshold(image1, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+    # image1 = cv2.adaptiveBilateralFilter(image1, (7, 7), 15)
+    # image1 = cv2.adaptiveThreshold(image1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 2)
+    # ret, image1 = cv2.threshold(image1, 110, 255, cv2.THRESH_BINARY_INV)
+    # image1 = cv2.Canny(image1, 100, 100, 3)
+
+    # element = cv2.getStructuringElement(cv2.MORPH_RECT, (29, 3))
+    # cv2.morphologyEx(image1, cv2.MORPH_OPEN, element)
+
+    # image1 = cv2.adaptiveThreshold(image1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 2)
+    cv2.namedWindow("Test")
+    cv2.imshow("Test", image1)
+    cv2.waitKey(0)
+    cv2.destroyWindow("Test")
+
+
+    #method 1
+    iplimage = cv.CreateImageHeader((width1,height1), cv.IPL_DEPTH_8U, channel1)
+    cv.SetData(iplimage, image1.tostring(),image1.dtype.itemsize * channel1 * (width1))
+    tesseract.SetCvImage(iplimage,api)
+
+    text=api.GetUTF8Text()
+    conf=api.MeanTextConf()
+    image=None
+    print "..............."
+    print "Ocred Text: %s"%text
+    print "Cofidence Level: %d %%"%conf
+
+    #method 2:
+    cvmat_image=cv.fromarray(image1)
+    iplimage =cv.GetImage(cvmat_image)
+    print iplimage
+
+    tesseract.SetCvImage(iplimage,api)
+    #api.SetImage(m_any,width,height,channel1)
+    text=api.GetUTF8Text()
+    conf=api.MeanTextConf()
+    image=None
+    print "..............."
+    print "Ocred Text: %s"%text
+    print "Cofidence Level: %d %%"%conf
+    api.End()
+
+def get_parts_of_image(img, rects):
+    # crops the detected recatngles in an image, and tries to find if there is text
+    for rect in rects:
+        xmin = 999999
+        xmax = 0
+        ymin = 999999
+        ymax = 0
+        for point in rect:
+            xmax = max(xmax, point[0][0])
+            ymax = max(ymax, point[0][1])
+            xmin = min(xmin, point[0][0])
+            ymin = min(ymin, point[0][1])
+        # print ("pecati " + str(xmin) + " " + str(xmax) + " " + str(ymin) + " " + str(ymax))
+        process_image(img[ymin:ymax, xmin:xmax])
 
 def main():
     print('Hello')
@@ -24,13 +137,19 @@ def main():
 
     images = load_images(image_names)
     for src in images:
-        detector = MorphologyTransformDetector(src)
-        rects = detector.find_rectangles()
-        display_rectangles(src, rects)
+        # detector = MorphologyTransformDetector(src)
+        # rects = detector.find_rectangles()
+        # display_rectangles(src, rects)
 
         detector2 = ThresholdBlurDetector(src)
-        rects2 = detector2.find_rectangles()
+        rects2 = detector2.find_rectangles_martin()
         display_rectangles(src, rects2)
+
+        get_parts_of_image(src, rects2)
+
+        # detector2 = ThresholdDetector(src)
+        # rects2 = detector2.find_rectangles()
+        # display_rectangles(src, rects2)
 
 if __name__ == '__main__':
     main()
