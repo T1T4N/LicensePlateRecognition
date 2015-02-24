9338a9cba8f2bf45813ad063f1984902e01f8602
Robert Armenski <r.armenski@gmail.com>
2015-02-24 10:34:10 +0100

    Add loader utils documentation

--- utils/loader.py
+++ utils/loader.py
@@ -4,7 +4,13 @@ import cv2
 
 
 def load_images(filenames):
-    """:returns a list of cv2::Mat objects that have been loaded from a list of filenames"""
+    """
+    Load images represented by an array of filenames
+
+    :param filenames: List of strings holding filenames of the images
+    :return: List of cv2::Mat objects representing the images
+    """
+
     ret = []
     for fn in filenames:
         ret.append(cv2.imread(fn))
@@ -12,8 +18,16 @@ def load_images(filenames):
 
 
 def get_images_from_dir(directory):
+    """
+    Get the names of every JPG image in a directory
+
+    :param directory: String representing an absolute or relative path to a directory
+    :return: List of strings containing names of the JPG images in the directory
+    """
+
     ret = []
-    for file_name in os.listdir(directory):
+    files = os.listdir(directory)
+    for file_name in files:
         if file_name.lower().endswith(".jpg"):
             ret.append(directory + os.sep + file_name)
     return ret
